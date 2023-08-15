0c19c4b56cddde0dbc514e3da0e5d427ffff129b
shigontsev <shigontsev1@mail.ru>
2023-08-15 14:52:35 +0400

    fix Sql query

---
+++ Task2_BD/SqlQuery_GetAll.sql
@@ -0,0 +1,17 @@
+﻿USE [Product_Task2]
+GO
+
+DECLARE @RC int
+
+-- TODO: задайте здесь значения параметров.
+
+EXECUTE @RC = [dbo].[GetAll] 
+GO
+
+
+-- Или просто запрос
+
+SELECT p.Name AS ProductN, c.Name AS CategoryN
+    FROM Product AS p
+	LEFT JOIN ProductAndCategory AS pc on p.Id = pc.Id_Product
+	LEFT JOIN Category AS c on c.Id = pc.Id_Category
